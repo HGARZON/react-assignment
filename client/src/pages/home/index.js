515a534227ed8f54d70ebb70b1ecb66f41ea4f20
Hernan Garzón <hernan.garzon@mercadolibre.com>
2018-11-15 15:43:16 -0300

    add https and linter fixes

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,22 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import headerImage from './assets/header.jpg';
 import './styles/home.css';
 
-class HomeView extends Component {
-  render() {
+export const HomeView = () =>  {
     return(
-      <div>
-        <h1 className="character-title">
-          Rick and Morty Character Information Database
-        </h1>
         <div>
-          <img className="header" src={headerImage} alt="Rick and Morty"/>
+            <h1 className="character-title">
+                Rick and Morty Character Information Database
+            </h1>
+            <div>
+                <img className="header" src={headerImage} alt="Rick and Morty"/>
+            </div>
+            <a href="https://rickandmortyapi.com" className="link-information">https://rickandmortyapi.com</a>
         </div>
-        <a href="https://rickandmortyapi.com" className="link-information">https://rickandmortyapi.com</a>
-      </div>
-    )
-  }
+    );
 }
-
-export default HomeView;
--- client/src/pages/table/components/CharacterRow.js
+++ client/src/pages/table/components/CharacterRow.js
@@ -3,75 +3,76 @@ import React, { Component } from 'react';
 import Modal from 'react-modal';
 
 const customStyles = {
-  content : {
-    top                   : '50%',
-    left                  : '50%',
-    right                 : 'auto',
-    bottom                : 'auto',
-    marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)'
-  }
+    content : {
+        top                   : '50%',
+        left                  : '50%',
+        right                 : 'auto',
+        bottom                : 'auto',
+        marginRight           : '-50%',
+        transform             : 'translate(-50%, -50%)'
+    }
 };
 class CharacterRow extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      modalIsOpen: false,
-      data: this.props
-    };
+        this.state = {
+            modalIsOpen: false,
+            data: this.props
+        };
 
-    this.openModal = this.openModal.bind(this);
-    this.afterOpenModal = this.afterOpenModal.bind(this);
-    this.closeModal = this.closeModal.bind(this);
-  }
+        this.openModal = this.openModal.bind(this);
+        this.afterOpenModal = this.afterOpenModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
+    }
 
-  openModal() {
-    this.setState({modalIsOpen: true});
-  }
+    openModal() {
+        this.setState({modalIsOpen: true});
+    }
 
-  afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    this.subtitle.style.color = '#f00';
-  }
+    afterOpenModal() {
+        // references are now sync'd and can be accessed.
+        this.subtitle.style.color = '#f00';
+    }
 
-  closeModal() {
-    this.setState({modalIsOpen: false});
-  }
+    closeModal() {
+        this.setState({modalIsOpen: false});
+    }
 
-  render() {
-      const {
-        image,
-        name,
-        species, 
-        status,
-        location
-      } = this.props.data;
-      return(
-      <tr className="character">
-        <td className="character__image"><img src={image} /></td>
-        <td className="character__name">{name}</td>
-        <td className="character__specie">{species}</td>
-        <td className="character__status">{status}</td>
-        <td className="character__location">{location.name}</td>
-        <td><button className="modalButton" onClick={this.openModal}>View details</button></td>
+    render() {
+        const {
+            image,
+            name,
+            species, 
+            status,
+            location
+        } = this.props.data;
 
-        <Modal
-          isOpen={this.state.modalIsOpen}
-          onAfterOpen={this.afterOpenModal}
-          onRequestClose={this.closeModal}
-          style={customStyles}
-          contentLabel={name}Modal
-        >
+        return(
+            <tr className="character">
+                <td className="character__image"><img src={image} /></td>
+                <td className="character__name">{name}</td>
+                <td className="character__specie">{species}</td>
+                <td className="character__status">{status}</td>
+                <td className="character__location">{location.name}</td>
+                <td><button className="modalButton" onClick={this.openModal}>View details</button></td>
 
-          <h2 ref={subtitle => this.subtitle = subtitle}>{name}</h2>
-          <div>I am a modal</div>
-          <button onClick={this.closeModal}>close</button>
-        </Modal>
+                <Modal
+                    isOpen={this.state.modalIsOpen}
+                    onAfterOpen={this.afterOpenModal}
+                    onRequestClose={this.closeModal}
+                    style={customStyles}
+                    contentLabel={name}Modal
+                    >
 
-      </tr>
-    );
-  }
+                    <h2 ref={subtitle => this.subtitle = subtitle}>{name}</h2>
+                    <div>I am a modal</div>
+                    <button onClick={this.closeModal}>close</button>
+                </Modal>
+
+            </tr>
+        );
+    }
 }
 
 export default CharacterRow;
